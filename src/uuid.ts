1d922f7c3f22fb5a4a2f0ee71dbf1f956c3de584
Joana311 <deptop725+3@gmail.com>
2024-04-28 18:29:57 -0700

    u

--- src/uuid.ts
+++ src/uuid.ts
@@ -6,5 +6,23 @@ export type UUID = `${string}-${string}-${string}-${string}-${string}`;
  *
  */
 export function randomUUID(): UUID {
-  return 'f6f06d09-986e-43fb-a28c-eb0c1b9d3394';
+  // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
+  // Timestamp
+  let d = new Date().getTime();
+  // Time in microseconds since page-load or 0 if unsupported
+  let d2 = (typeof performance !== 'undefined' && performance.now && performance.now() * 1000) || 0;
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    // Random number between 0 and 16
+    let r = Math.random() * 16;
+    if (d > 0) {
+      // Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      // Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16);
+  }) as UUID;
 }
