f63cd83f6dbcac99cce30b994faa4679a2830b82
Joana311 <deptop725+3@gmail.com>
2024-05-01 16:35:10 -0700

    u

--- src/story-plan-organizer.ts
+++ src/story-plan-organizer.ts
@@ -235,7 +235,6 @@ if (inputImportFileElement) {
         //console.log(event.target.result);
         const jsonString = event.target.result as string;
         if (validJson(jsonString)) {
-          console.log('\n\n\n\nPOINT_C');
           const obj = JSON.parse(jsonString);
           console.log(obj);
           load(obj as Dto, state);
